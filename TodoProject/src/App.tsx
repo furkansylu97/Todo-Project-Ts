e38e7d2f23b6b22b87cf26b816e27a3706ea8183
Furkan Soylu <furkansylu97@gmail.com>
2025-05-05 21:41:29 +0300

    Checking of values ​​in input tags with usestate method was performed

--- TodoProject/src/App.tsx
+++ TodoProject/src/App.tsx
@@ -1,13 +1,26 @@
-import React, { FC } from 'react'
+import React, { type FC, type ChangeEvent } from 'react'
 import './App.css'
+import { useState } from 'react'
 
-const App: FC = () {
+const App: FC = () => {
+
+  const [task, setTask] = useState<string>('')
+  const [duration, setDuration] = useState<number>(0)
+
+  const handleChange = (event :ChangeEvent<HTMLInputElement> ) => {
+    if (event.target.name === 'task') {
+      setTask(event.target.value)
+    }
+    else {
+      setDuration(Number(event.target.value))
+    }
+  }
 
   return (
     <>
      <div>
-        <input type="text" placeholder='Please enter your task...' />
-        <input type="number" placeholder='How many days should you complete?' />
+        <input type="text" onChange={handleChange} value={task} name='task' placeholder='Please enter your task...' />
+        <input type="number" onChange={handleChange} value={duration} name='duration' placeholder='How many days should you complete?' />
         <button>Add a new tasd</button>
     </div>
     </>

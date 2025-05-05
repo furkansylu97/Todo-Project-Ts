5bd4def5c2f80b0e81dee1f0fc5c0ecbe3037701
Furkan Soylu <furkansylu97@gmail.com>
2025-05-05 22:10:42 +0300

    New task adding methods and type definition were performed

--- TodoProject/src/App.tsx
+++ TodoProject/src/App.tsx
@@ -1,11 +1,18 @@
 import React, { type FC, type ChangeEvent } from 'react'
 import './App.css'
 import { useState } from 'react'
+import type { todoType } from './components/Apptypes'
 
 const App: FC = () => {
 
+  // type todoType = {
+  //   taskName:string;
+  //   duration:number;
+  // }
+
   const [task, setTask] = useState<string>('')
   const [duration, setDuration] = useState<number>(0)
+  const [todoList, setTodoList] = useState<todoType[]>([])
 
   const handleChange = (event :ChangeEvent<HTMLInputElement> ) => {
     if (event.target.name === 'task') {
@@ -16,12 +23,17 @@ const App: FC = () => {
     }
   }
 
+  const addNewTask = () => {
+    const newTask = {taskName:task, duration:duration};
+    setTodoList([...todoList, newTask])
+  }
+
   return (
     <>
      <div>
         <input type="text" onChange={handleChange} value={task} name='task' placeholder='Please enter your task...' />
         <input type="number" onChange={handleChange} value={duration} name='duration' placeholder='How many days should you complete?' />
-        <button>Add a new tasd</button>
+        <button onClick={addNewTask}>Add a new tasd</button>
     </div>
     </>
   )

c819d4a90dc241e42dd5647cb82d79803f39d020
Furkan Soylu <furkansylu97@gmail.com>
2025-05-05 21:07:58 +0300

    The app component was defined as a functional component (FC) and the contents were written

--- TodoProject/src/App.tsx
+++ TodoProject/src/App.tsx
@@ -1,10 +1,15 @@
+import React, { FC } from 'react'
 import './App.css'
 
-function App() {
+const App: FC = () {
 
   return (
     <>
-    Todo Project
+     <div>
+        <input type="text" placeholder='Please enter your task...' />
+        <input type="number" placeholder='How many days should you complete?' />
+        <button>Add a new tasd</button>
+    </div>
     </>
   )
 }
